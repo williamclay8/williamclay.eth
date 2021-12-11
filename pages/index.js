dbb3040f4db119aeb591b29c9c1f4d1be6ed3fad
williamclay8 <93858823+williamclay8@users.noreply.github.com>
2021-12-11 08:45:36 -0600

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          williamclay.eth <a href="https://nextjs.org"></a>
         </h1>
 
         <p className={styles.description}>
